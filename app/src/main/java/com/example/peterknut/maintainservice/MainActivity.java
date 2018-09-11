6a9feac56a47a9884aafc7dbfa67965fcac5c5dd
peterknut <peterpeng1191@gmail.com>
2018-09-12 02:30:28 +0800

    个人信息页面 定义了整个app的风格

--- app/src/main/java/com/example/peterknut/maintainservice/MainActivity.java
+++ app/src/main/java/com/example/peterknut/maintainservice/MainActivity.java
@@ -8,7 +8,6 @@ import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.widget.TextView;
-
 public class MainActivity extends AppCompatActivity {
 
 
--- app/src/main/java/com/example/peterknut/maintainservice/MyInformationFragment.java
+++ app/src/main/java/com/example/peterknut/maintainservice/MyInformationFragment.java
@@ -1,25 +1,53 @@
 package com.example.peterknut.maintainservice;
 
 import android.content.Context;
+import android.graphics.drawable.Drawable;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
+import android.widget.Button;
 
 
 public class MyInformationFragment extends Fragment {
 
+    //按钮
+    private Button myNameButton;
+    private Button historyOrderButton;
+    private Button aboutTianduanButton;
+    private Button helpButton;
+    private Button settingButton;
+
+    //视图
+    private View view;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_my_information, container, false);
+        view = inflater.inflate(R.layout.fragment_my_information, container, false);
+
+        myNameButton = view.findViewById(R.id.myNameButton);
+        historyOrderButton = view.findViewById(R.id.historyOrderButton);
+        aboutTianduanButton = view.findViewById(R.id.aboutTianDuanButton);
+        helpButton = view.findViewById(R.id.helpButton);
+        settingButton = view.findViewById(R.id.settingButton);
+
+        setDrawable();
+        return view;
     }
 
+
+    //设置按钮左边图片大小
+    public void setDrawable(){
+
+        Drawable profilePhoto = getResources().getDrawable(R.drawable.profile_photo);
+        //前两个表示坐标位置，长宽
+        profilePhoto.setBounds(20, 10, 230,240);
+        myNameButton.setCompoundDrawables(profilePhoto,null,null,null);
+
+    }
 
 
 }
